293fd1ff8741b1298d97b1c38eb1a99436472b82
Marina Rozova <marinar578@gmail.com>
2015-12-23 09:21:50 -0500

    add a few more comments to js

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 
 $(function () {
 
-    console.log("doc ready");
-
+    //array holding images of cards (these are named by their index in initialDeck but are not yet sorted)
     var cardImages = ["images/00_heart_K.svg", "images/14_diamond_A.svg", "images/28_club_2.svg", "images/42_spade_3.svg", "images/01_heart_A.svg", "images/15_diamond_2.svg", "images/29_club_3.svg", "images/43_spade_4.svg", "images/02_heart_2.svg", "images/16_diamond_3.svg", "images/30_club_4.svg", "images/44_spade_5.svg", "images/03_heart_3.svg", "images/17_diamond_4.svg", "images/31_club_5.svg", "images/45_spade_6.svg", "images/04_heart_4.svg", "images/18_diamond_5.svg", "images/32_club_6.svg", "images/46_spade_7.svg", "images/05_heart_5.svg", "images/19_diamond_6.svg", "images/33_club_7.svg", "images/47_spade_8.svg", "images/06_heart_6.svg", "images/20_diamond_7.svg", "images/34_club_8.svg", "images/48_spade_9.svg", "images/07_heart_7.svg", "images/21_diamond_8.svg", "images/35_club_9.svg", "images/49_spade_10.svg", "images/08_heart_8.svg", "images/22_diamond_9.svg", "images/36_club_10.svg", "images/50_spade_J.svg", "images/09_heart_9.svg", "images/23_diamond_10.svg", "images/37_club_J.svg", "images/51_spade_Q.svg", "images/10_heart_10.svg", "images/24_diamond_J.svg", "images/38_club_Q.svg", "images/11_heart_J.svg", "images/25_diamond_Q.svg", "images/39_spade_K.svg", "images/12_heart_Q.svg", "images/26_club_K.svg", "images/40_spade_A.svg", "images/13_diamond_K.svg", "images/27_club_A.svg", "images/41_spade_2.svg"];
 
+    //this sorts the above array so that the indices of the images line up with the indices of their cards in initialDeck
     var sortedImages = cardImages.sort();
 
+    //creates deck of cards and corresponds to indices of images in above arrays
     var initialDeck = [];
 
-    //the following variables keep track of the amount  of points the player and dealer have
+    //populates the original deck array
+    for(var i=0; i<52; i++){
+        initialDeck.push(i)
+    };
+
+    //the following variables keep track of the amount  of points and wins the player and dealer have
     var playerPoints = 0;
     var dealerPoints = 0;
 
@@ -18,16 +24,12 @@ $(function () {
     var dealerWins = 0;
     var ties = 0;
 
-    //use these to keep track of each player's hand as cards are dealt (for ace conditionals)
+    //use these to keep track of each player's hand and values of cards in the hands as cards are dealt (for ace conditionals)
     var playerHand = [];
-      var playerValues = [];
-    var dealerHand = [];
-        var dealerValues = [];
+    var playerValues = [];
 
-    //populates the original deck array
-    for(var i=0; i<52; i++){
-        initialDeck.push(i)
-    };
+    var dealerHand = [];
+    var dealerValues = [];
 
     //calculates point value of a card
     //0 = King, 1=Ace, 11=Jack, 12=Queen, all other card values = their numbers
@@ -61,7 +63,7 @@ $(function () {
         return deck;
     }
 
-    //deals cards from shuffled deck and returns array of [card point value, card name, card suit]
+    //deals cards from shuffled deck
     var deal = function(player){
 
         var card = initialDeck.pop();
@@ -160,7 +162,7 @@ $(function () {
         $('.stand').hide();
     }
 
-    //serve out first four cards of the game
+    //serves out first four cards of the game
     var initialDeal = function(){
         shuffleDeck(initialDeck);
         deal("dealer");
@@ -176,18 +178,18 @@ $(function () {
         };
      }
 
-    //start the game with the play button, but hide the hit and stand buttons initially
+    //starts the game with the play button, but hides the hit and stand buttons initially
     hideButtons();
     $('.decks').hide();
     $('.play-again').hide();
     var playGame = function(){
         $('.play').show();
         $('.play-again').hide();
-  }
-
+    }
 
     $('.play').click(initialDeal);
 
+    //returns used cards to initial deck and resets hand and value arrays
     var resetHandsAndDeck = function(){
         playerHand.forEach(function(el){
             initialDeck.push(el);
@@ -201,7 +203,8 @@ $(function () {
         dealerValues=[];
     };
 
-
+    //resets all points to 0, removes divs containing cards in each hand,
+    //hides hit and stand buttons and removes score display
     var clearTable = function(){
         playerPoints = 0;
         dealerPoints = 0;
@@ -226,6 +229,7 @@ $(function () {
 
     };
 
+    //event listener for "hit" button
     $('.hit').click(function(){
             deal("player");
             if(playerPoints>21){
@@ -235,7 +239,7 @@ $(function () {
             }
     });
 
-
+    //event listener for "stand" button
     $('.stand').click(function(){
         while(dealerPoints<17){
             deal("dealer");
